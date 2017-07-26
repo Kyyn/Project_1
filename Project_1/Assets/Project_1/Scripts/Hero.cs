00dc8207f2113a174045ee83b00a43a9334ec4ed
DESKTOP-681L4K3\robfe <orobfer@gmail.com>
2017-07-26 17:04:03 +0200

    End_Section 147

--- Project_1/Assets/Project_1/Scripts/Hero.cs
+++ Project_1/Assets/Project_1/Scripts/Hero.cs
@@ -4,16 +4,67 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+    public float rotateSpeed = 100.0f;
+    public int magicOrbAmount = 20;
+    public GameObject magicOrb = null;
+    public Transform socket = null;
+    public int health = 100;
 
-	// Use this for initialization
 	void Start ()
 	{
 		
 	}
 
-	// Update is called once per frame
 	void Update ()
 	{
-		
+        Move();
+        Shoot();
 	}
+
+    private void Move()
+    {
+        float move = Input.GetAxis("Vertical") * moveSpeed;
+        float rotation = Input.GetAxis("Horizontal") * rotateSpeed;
+        this.transform.Translate(0, 0, move * Time.deltaTime);
+        this.transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+
+    void Shoot()
+    {
+        if ( Input.GetButtonDown("Fire1"))
+        {
+            if (magicOrbAmount > 0)
+            {
+                magicOrbAmount--;
+                GameObject obj = Instantiate(magicOrb, socket.position, socket.rotation) as GameObject;
+                obj.name = "magicOrb";
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "cannonball")
+        {
+            int hp = other.GetComponent<Cannonball>().hitpoint;
+            GetHealth(hp);
+        }
+    }
+
+    void GetHealth(int hp)
+    {
+        if (health > 0)
+        {
+            health -= hp;
+
+            Debug.Log("Hero hit for: " + hp + "  Hero Health: " + health);
+            
+        }
+        else
+        {
+            Debug.Log("Game Over.");
+        }
+    }
 }
--- Project_1/Assets/Project_1/Scripts/Turret.cs
+++ Project_1/Assets/Project_1/Scripts/Turret.cs
@@ -4,16 +4,73 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public GameObject cannonball = null;
+    public Transform player = null;
 
-	// Use this for initialization
+    public int health = 100;
+
+    public float minDelay = 1.0f;
+    public float maxDelay = 4.0f;
+
+    private float lastTime = 0.0f;
+    private float delayTime = 0.0f;
+
 	void Start ()
 	{
 		
 	}
 
-	// Update is called once per frame
+	
 	void Update ()
 	{
-		
+        FollowPlayer();
+        Shoot();
 	}
+
+    void FollowPlayer()
+    {
+        this.transform.LookAt(player);
+    }
+
+    void Shoot()
+    {
+        if (Time.time > lastTime + delayTime)
+        {
+            lastTime = Time.time;
+
+            delayTime = GetRandomValue();
+
+            GameObject obj = Instantiate(cannonball, this.transform.position, this.transform.rotation) as GameObject;
+            obj.name = "cannonball";
+        }
+    }
+
+    float GetRandomValue()
+    {
+        return Random.Range(minDelay, maxDelay);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "magicOrb")
+        {
+            int hp = other.GetComponent<MagicOrb>().hitpoint;
+            GetHealth(hp);
+        }
+    }
+
+    void GetHealth(int hp)
+    {
+        if (health > 0)
+        {
+            health -= hp;
+
+            Debug.Log("Turrent hit for: " + hp + "  Turrent Health: " + health);
+
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
